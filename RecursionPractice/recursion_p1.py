dd157922542b3f0f9f21743291dfea45549640a0
Efe Berke <efeberkev@gmail.com>
2025-11-09 19:13:44 +0300

    switching the variable from list to int

--- RecursionPractice/recursion_p1.py
+++ RecursionPractice/recursion_p1.py
@@ -7,13 +7,17 @@ Input: [3, [7, [1, 9], 4], 10, [2, [5, 11]]], threshold = 5
 Output: 7 + 9 + 10 + 11 = 37
 """
 
-def recursion_sum(lst_of_lsts, sum = [0]):
+def recursion_sum(lst_of_lsts, sum = 0):
+
     for data in lst_of_lsts:
-        if type(data) == list:
-            recursion_sum(data, sum)
+
+        if type(data) == list: # or equivalently: if isinstance(data, list):
+            sum += recursion_sum(data, sum = 0)
+
         elif data > 5:
-            sum[0] += data
-    return sum[0]
+            sum += data
+
+    return sum
 
 
  # Test Lists
@@ -22,7 +26,7 @@ test_list2 = [1, [4, [6, 8]], 3, [10, [2, [15]]]] # 39
 test_list3 = [[5, [5, [5]]], [4, [6, [7, [8]]]]] # 21
 test_list4 = [[], [1, [2, [3]]], [[[[10]]]], 9] # 19
 
-print(recursion_sum(test_list1, [0]))
-print(recursion_sum(test_list2, [0]))
-print(recursion_sum(test_list3, [0]))
-print(recursion_sum(test_list4, [0]))
+print(recursion_sum(test_list1))
+print(recursion_sum(test_list2))
+print(recursion_sum(test_list3))
+print(recursion_sum(test_list4))
