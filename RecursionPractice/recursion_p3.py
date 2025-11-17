b2ad0378cd18349f744209338692ac130d46d682
Efe Berke <efeberkev@gmail.com>
2025-11-17 19:57:41 +0300

    faster pow(x, n) function added to recursion_p3.py

--- RecursionPractice/recursion_p3.py
+++ RecursionPractice/recursion_p3.py
@@ -19,3 +19,27 @@ def isPowerOfThree(n):
 
 print(isPowerOfThree(-1))
 print(mypow(2, 5))
+
+
+def fastpow(x, n):
+    # base case
+    if n == 0:
+        return 1
+
+    # negative power
+    if n < 0:
+        return 1 / fastpow(x, -n)
+
+    # even power
+    if n % 2 == 0:
+        half = fastpow(x, n // 2)
+        return half * half
+
+    # odd power
+    else:
+        return x * fastpow(x, n-1)
+
+
+print(fastpow(2, -3))
+print(fastpow(5, 0))
+print(fastpow(2, 10))
